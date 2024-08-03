f5d6fc96af139a72558527b835d1c09a12dd893e
Arijit Chongder <39260192+chongder6@users.noreply.github.com>
2024-08-04 00:07:31 +0530

    Add files via upload

---
+++ Best_time_sell_2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int sell_buy(int arr[], int size){
+    int profit = 0;
+
+    for(int i= 1; i<size; i++){
+        if(arr[i] > arr[i-1]){
+            profit += (arr[i]- arr[i-1]);
+    
+        }
+    }
+    return profit;
+}
+int main(){
+    int n;
+    cin>>n;
+    int array[n];
+    for(int i=0; i<n; i++){
+        cin>>array[i];
+    }
+
+    cout<<sell_buy(array, n)<<endl;
+
+    return 0;
+}
